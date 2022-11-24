10274134d105a51b1100c0415ebe896e8e50b318
Aditya Chandhoke <adipusk@gmail.com>
2022-11-24 12:08:11 +0530

    Added R Squared error for accuracy.

--- Regression/Regression_scratch.py
+++ Regression/Regression_scratch.py
@@ -18,12 +18,26 @@ def best_fit_slope(xs,ys):
     b = mean(ys) - m*mean(xs)
     return m, b
 
+
+def sq_error(ys_orig, ys_line):
+    return sum((ys_line-ys_orig)**2)
+
+def coef_deter(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    sq_error_reg = sq_error(ys_orig, ys_line)
+    sq_error_y_mean = sq_error(ys_orig, y_mean_line)
+    return 1-(sq_error_reg/sq_error_y_mean)
+
+
 m, b = best_fit_slope(xs,ys)
 
 regression_line = [(m*x)+b for x in xs]
 
 predict_x = 8
 predict_y = (m*predict_x) + b
+
+r_sq = coef_deter(ys, regression_line)
+print(r_sq)
 
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y, color='g')
