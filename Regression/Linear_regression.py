e76578064d0d7e282c5560f62e04a464f346fa04
Aditya <adipusk@gmail.com>
2024-02-29 20:08:58 +0530

    Add example for Regression

--- Regression/Linear_regression.py
+++ Regression/Linear_regression.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 class LinearRegression():
     def __init__(self, alpha, iter) -> None:
@@ -7,18 +7,21 @@ class LinearRegression():
         self.iter = iter # No of Iterations
         self.weight = None
         self.bias = None
+        self.y_pred = None
     
     def fit(self, X, y):
+        X = np.array(X)
         n_samples, n_features = X.shape
         self.weights = np.random.rand(n_features)
         self.bias = 0
+        self.y_pred = 0
 
         for _ in range(self.iter):
 
-            y_pred = np.dot(X, self.weights) + self.bias
+            self.y_pred = np.dot(X, self.weights) + self.bias
 
-            dw = (1/n_samples) * np.dot(X.T, y_pred - y)
-            db = (1/n_samples) * np.sum(y_pred-y)
+            dw = (1/n_samples) * np.dot(X.T, self.y_pred - y)
+            db = (1/n_samples) * np.sum(self.y_pred-y)
 
             self.weights -= self.alpha * dw
             self.bias -= self.alpha * db
@@ -28,15 +31,15 @@ class LinearRegression():
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
     
-    def show_line(self, X, y, y_pred):
+    def show_line(self,X ,y ,y_pred):
         '''
         X - From the dataset
         y - From the dataset
-        y_pred - Returned by the predict function
+        y_pred - Declared in the init fn
         '''
-        plt.scatter( X, y, color = 'blue' ) 
+        plt.scatter(X, y, color = 'blue' ) 
       
-        plt.plot( X, y_pred, color = 'orange' ) 
+        plt.plot(X, y_pred, color = 'orange' ) 
       
         plt.title( 'Line adjusted to data' ) 
       
@@ -47,3 +50,6 @@ class LinearRegression():
         plt.show() 
 
         return self
+
+if __name__ == '__main__':
+    pass
---
+++ rough.py
@@ -0,0 +1,7 @@
+from Regression import Linear_regression
+from sklearn.datasets import make_regression
+
+X, y = make_regression(n_samples=100, n_features=1, noise=15, random_state=21)
+lr = Linear_regression.LinearRegression(alpha=0.1, iter=100)
+lr.fit(X,y)
+lr.show_line(X,y,lr.y_pred)
