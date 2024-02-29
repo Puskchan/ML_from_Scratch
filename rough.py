53b058c2b73cc09e116107b3320908fc4e63c371
Aditya <adipusk@gmail.com>
2024-02-29 20:39:35 +0530

    update rough

--- rough.py
+++ rough.py
@@ -4,4 +4,6 @@ from sklearn.datasets import make_regression
 X, y = make_regression(n_samples=100, n_features=1, noise=15, random_state=21)
 lr = Linear_regression.LinearRegression(alpha=0.1, iter=100)
 lr.fit(X,y)
+pred = lr.predict(X=[3])
+print(pred)
 lr.show_line(X,y,lr.y_pred)
